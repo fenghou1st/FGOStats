f6f16270cdb832cb87bedf47771235e42fff415e
fenghou <fenghou1st@gmail.com>
2017-01-19 16:03:45 +0800

    chore(*): add scripts to build the project

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ bin/console cache:clear --env=prod
 
 npm run build
 rm -rf *.json.gzip
+rm -rf var/cache/* var/logs/* var/sessions/*
 
 tail -100 ~/php/logs/error_fgo_stats.log
 tail -100 ~/php/logs/access_fgo_stats.log
